0e9095fb01b35a4bae9464bfd9137d6d97f57166
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-08-29 15:01:31 +0000

    task answer

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - Deletes a binary tree
+ * @tree: the tree to be deleted
+ * Return: void
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *node = tree;
+
+	if (node == NULL)
+		return;
+	if (node->right == NULL && node->left == NULL)
+		free(node);
+	binary_tree_delete(node->left);
+	binary_tree_delete(node->right);
+}
