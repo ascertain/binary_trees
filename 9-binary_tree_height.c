25903ca0dcaf2b052c58418f6cced934279a9a00
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-08-29 16:19:56 +0000

    task answer

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,16 +7,16 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right;
-	if (tree->parent == NULL && tree == NULL)
-		return (0);
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 	return (max(left, right) + 1);
 }
-size_t max(a, b)
+size_t max(size_t a, size_t b)
 {
 	return (a > b) ? a : b;
 }
