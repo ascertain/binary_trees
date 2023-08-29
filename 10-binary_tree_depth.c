60aea0e16c79fcbca1e1334519f437d9cb3a0443
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-08-29 16:36:21 +0000

    task answer

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,9 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - measures the depth of a tree from root node to the target node
+ * @tree: the node at which depth would be measured
+ * Return: 0 if null or leaf, otherwise, the height
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+
