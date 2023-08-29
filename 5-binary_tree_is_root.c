8581ed10a48aef09fce459d246bf0e47d70ccec3
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-08-29 15:34:07 +0000

    task answer

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - Checks if a tree is a root (parentless)
+ * @node: the node to be checked
+ * Return: 1 if root, 0 if otherwise
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->parent == NULL)
+		return (1);
+	return (0);
+}
