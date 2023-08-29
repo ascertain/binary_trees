297f4eaff86751ec8afa550193191c6cf9748c4d
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-08-29 21:27:12 +0000

    task answer

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,14 +20,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		og->parent = node;
 		node->left = og;
 		node->parent = parent;
-		node->right = NULL;
 	}
 	else
 	{
 		parent->left = node;
 		node->parent = parent;
 		node->left = NULL;
-		node->right = NULL;
 	}
 	return (node);
 }
