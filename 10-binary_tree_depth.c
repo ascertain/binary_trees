45240e9b80a7efb6951e91aa2d43e0b4f014d119
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-08-29 20:47:49 +0000

    task answer

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_depth - measures the depth of a tree from root node to the target node
+ * binary_tree_depth - measures depth of a tree from root node to target node
  * @tree: the node at which depth would be measured
  * Return: 0 if null or leaf, otherwise, the height
  */
@@ -11,5 +11,5 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree->parent == NULL)
 		return (0);
 	else
-		return(binary_tree_depth(tree->parent) + 1);
+		return (binary_tree_depth(tree->parent) + 1);
 }
