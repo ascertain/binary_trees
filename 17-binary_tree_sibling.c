5618fab54784d1ab235e40fd018ed237ca1f5fc1
ascertain <kas.naya@gmail.com>
2023-08-30 10:16:09 -0700

    update task

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,13 +3,16 @@
  * binary_tree_sibling - finds the sibling of the node
  * @node: the node to be checked for siblings existence
  * Return: the sibling node
- */binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	binary_tree_t *parent = node->parent;
 
-	if (node == NULL || node->parent == NULL)
+	if (node == NULL || parent == NULL)
 		return (NULL);
-	if (node->parent->left == node)
-		return (node->parent->right);
-	return (node->parent->left);
+	if ((parent->left == node) && (parent->right != NULL))
+		return (parent->right);
+	if ((parent->right == node) && (parent->left != NULL))
+		return (parent->left);
+	return (NULL);
 }
