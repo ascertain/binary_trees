baff70a82c51f74ba5b77eb088ccb03291b1536b
ascertain <kas.naya@gmail.com>
2023-08-30 10:04:35 -0700

    update task

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_sibling - finds the sibling of the node
- * @node: the node to be checked for siblings existence
- * Return: the sibling node
+ * binary_tree_sibling - Finds the sibling of a
+ *                       node in a binary tree.
+ * @node: A pointer to the node to find the sibling of.
+ *
+ * Return: If node is NULL or there is no sibling - NULL.
+ *         Otherwise - a pointer to the sibling.
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
-
-	if (node == NULL || parent == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	if ((parent->left == node) && (parent->right != NULL))
-		return (parent->right);
-	if ((parent->right == node) && (parent->left != NULL))
-		return (parent->left);
-	return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,29 +1,20 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_uncle - finds the uncle of the node
- * @node: the node to check their uncle
- * Return: NULL if no uncle or error, otherwise, the uncle
+ * binary_tree_uncle - Finds the uncle of a node
+ *                     in a binary tree.
+ * @node: A pointer to the node to find the uncle of.
+ *
+ * Return: If node is NULL or has no uncle, NULL.
+ *         Otherwise, a pointer to the uncle node.
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
-
-	return (binary_tree_sibling(parent));
-}
-/**
- * binary_tree_sibling - finds the sibling of the node
- * @node: the node to be checked for siblings existence
- * Return: the sibling node
- */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
-{
-	binary_tree_t *parent = node->parent;
-
-	if (node == NULL || parent == NULL)
+	if (node == NULL ||
+	    node->parent == NULL ||
+	    node->parent->parent == NULL)
 		return (NULL);
-	if ((parent->left == node) && (parent->right != NULL))
-		return (parent->right);
-	if ((parent->right == node) && (parent->left != NULL))
-		return (parent->left);
-	return (NULL);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
