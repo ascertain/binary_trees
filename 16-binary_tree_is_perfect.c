25b7db131c62374b1b4241de73df7a63018e099a
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-08-30 06:00:40 +0000

    task answer

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,25 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_is_perfect - checks if a node has 2 children for each ancestor
+ * @tree: the root node
+ * Return: 0 if not perfect, 1 if perfect
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (binary_tree_balance(tree) != 0)
+		return (0);
+	if ((tree->left && tree->right))
+		return (
+			binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right)
+		);
+	return (0);
+}
+/**
  * binary_tree_balance - calculates the balance factor (left to right)
  * @tree: the root node to be measured
  * Return: 0 if null tree, otherwise, the factor number
