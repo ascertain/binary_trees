2fe0a5e0a021243a7cd4f149f68a22485281b41b
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-08-30 06:16:45 +0000

    task answer

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,16 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_uncle - finds the uncle of the node
+ * @node: the node to check their uncle
+ * Return: NULL if no uncle or error, otherwise, the uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *parent = node->parent;
+
+	return (binary_tree_sibling(parent));
+}
+/**
  * binary_tree_sibling - finds the sibling of the node
  * @node: the node to be checked for siblings existence
  * Return: the sibling node
